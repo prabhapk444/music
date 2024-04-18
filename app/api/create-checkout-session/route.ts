2fdd52eb37a4c3b2ac62846bccb8bd6bd372c939
prabhapk444 <karanprabha22668@gmail.com>
2024-04-19 00:09:52 +0530

    a

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -12,21 +12,18 @@ export async function POST(
   const { price, quantity = 1, metadata = {} } = await request.json();
 
   try {
-    const supabase = createRouteHandlerClient({ 
-      cookies
-      });      const {
+    const supabase = createRouteHandlerClient({ cookies });
+    const {
       data: { user }
     } = await supabase.auth.getUser();
 
-    const customer = await createOrRetrieveCustomer({
+    const customerID = await createOrRetrieveCustomer({
       uuid: user?.id || '',
       email: user?.email || ''
     });
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
-      billing_address_collection: 'required',
-      customer,
       line_items: [
         {
           price: price.id,
@@ -34,16 +31,14 @@ export async function POST(
         }
       ],
       mode: 'subscription',
-      allow_promotion_codes: true,
+      customer: customerID, 
       subscription_data: {
-        trial_from_plan: true,
         metadata
       },
       success_url: `${getURL()}/account`,
       cancel_url: `${getURL()}/`
     });
-
-    return NextResponse.json({ sessionId: session.id });
+    
   } catch (err: any) {
     console.log(err);
     return new NextResponse('Internal Error', { status: 500 });
